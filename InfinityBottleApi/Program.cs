2659e230c4e64e318197519ad6476bfaa544935d
r3d5un <r3d5un@pm.me>
2022-12-25 17:42:48 +0100

    Add InifinityBottleApi project

---
+++ InfinityBottleApi/Program.cs
@@ -0,0 +1,47 @@
+using DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Logging
+var logger = new LoggerConfiguration().ReadFrom
+    .Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add DatabaseContext
+builder.Services.AddDbContext<DatabaseContext>(
+    options =>
+        options.UseNpgsql(
+            // Do not use this connection string in production!
+            "host=localhost;port=5432;database=postgres;username=postgres;password=postgres"
+        )
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
