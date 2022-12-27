16846ee5f2f81dd5291b1921a020d1217d8a39ee
r3d5un <r3d5un@pm.me>
2022-12-27 18:56:29 +0100

    Always enable Swagger

--- InfinityBottleApi/Program.cs
+++ InfinityBottleApi/Program.cs
@@ -37,11 +37,8 @@ builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
