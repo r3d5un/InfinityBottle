67e2efe140df36cc3eb93caa59d4ee2b542a9f47
r3d5un <r3d5un@pm.me>
2022-12-27 19:21:28 +0100

    Add Exception Handling to CountryController.cs

--- InfinityBottleApi/Controllers/CountryController.cs
+++ InfinityBottleApi/Controllers/CountryController.cs
@@ -8,6 +8,7 @@ namespace InfinityBottleApi.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
+[ProducesResponseType(StatusCodes.Status500InternalServerError)]
 public class CountryController : ControllerBase
 {
     private readonly IUnitOfWork _unitOfWork;
@@ -26,49 +27,108 @@ public class CountryController : ControllerBase
     }
 
     [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<CountryDto>), 200)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> Get()
     {
-        var countries = await _unitOfWork.Countries.GetAllAsync();
-        var result = _mapper.Map<IEnumerable<CountryDto>>(countries);
-        return Ok(result);
+        try
+        {
+            var countries = await _unitOfWork.Countries.GetAllAsync();
+            var result = _mapper.Map<IEnumerable<CountryDto>>(countries);
+            if (!result.Any())
+            {
+                return NotFound("No countries found");
+            }
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("Exception: {Message}", ex.Message);
+            return StatusCode(500, "Internal server error");
+        }
     }
 
     [HttpGet("{id}")]
+    [ProducesResponseType(typeof(CountryDto), 200)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> Get(string id)
     {
-        var country = await _unitOfWork.Countries.GetAsync(id);
-        var result = _mapper.Map<CountryDto>(country);
-        return Ok(result);
+        try
+        {
+            var country = await _unitOfWork.Countries.GetAsync(id);
+            var result = _mapper.Map<CountryDto>(country);
+            if (result == null)
+            {
+                return NotFound("Country not found");
+            }
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("Exception: {Message}", ex.Message);
+            return StatusCode(500, "Internal server error");
+        }
     }
 
     [HttpPost]
+    [ProducesResponseType(typeof(CountryDto), 201)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> Post([FromBody] CountryDto countryDto)
     {
-        var country = _mapper.Map<Country>(countryDto);
-        await _unitOfWork.Countries.AddAsync(country);
-        await _unitOfWork.CompleteAsync();
-        return Ok();
+        try
+        {
+            var country = _mapper.Map<Country>(countryDto);
+            await _unitOfWork.Countries.AddAsync(country);
+            await _unitOfWork.CompleteAsync();
+            return Created("Country created", country);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("Exception: {Message}", ex.Message);
+            return StatusCode(500, "Internal server error");
+        }
     }
 
     [HttpPut("{id}")]
+    [ProducesResponseType(typeof(CountryDto), 200)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> Put(string id, [FromBody] CountryDto countryDto)
     {
-        var country = await _unitOfWork.Countries.GetAsync(id);
-        _mapper.Map(countryDto, country);
-        await _unitOfWork.CompleteAsync();
-        return Ok();
+        try
+        {
+            var country = await _unitOfWork.Countries.GetAsync(id);
+            _mapper.Map(countryDto, country);
+            await _unitOfWork.CompleteAsync();
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("Exception: {Message}", ex.Message);
+            return StatusCode(500, "Internal server error");
+        }
     }
 
     [HttpDelete("{id}")]
+    [ProducesResponseType(typeof(CountryDto), 200)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> Delete(string id)
     {
-        Country? countryToDelete = await _unitOfWork.Countries.GetAsync(id);
-        if (countryToDelete is null)
+        try
         {
-            return NotFound();
+            Country? countryToDelete = await _unitOfWork.Countries.GetAsync(id);
+            if (countryToDelete is null)
+            {
+                return NotFound();
+            }
+
+            _unitOfWork.Countries.RemoveAsync(countryToDelete);
+            await _unitOfWork.CompleteAsync();
+            return Ok();
         }
-        _unitOfWork.Countries.RemoveAsync(countryToDelete);
-        await _unitOfWork.CompleteAsync();
-        return Ok();
+        catch (Exception ex)
+        {
+            _logger.LogError("Exception: {Message}", ex.Message);
+            return StatusCode(500, "Internal server error");
+        }
     }
 }
